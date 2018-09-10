26bd1c358b5a1e2badbe836392079d96eb8917bb
Bianca Jemsten <biancajemsten@gmail.com>
2018-09-10 13:47:17 +0100

    added Link to Schema and added resolver for it

--- src/index.js
+++ src/index.js
@@ -4,13 +4,33 @@ const { GraphQLServer } = require('graphql-yoga');
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `;
 
-// 2
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}];
+
 const resolvers = {
   Query: {
-    info: () => 'This is the API of a Hackernews Clone'
+    info: () => 'This is the API of a Hackernews Clone',
+    feed: () => links
+  },
+
+  // 3
+  Link: {
+    id: (root) => root.id,
+    description: (root) => root.description,
+    url: (root) => root.url
   }
 };
 
